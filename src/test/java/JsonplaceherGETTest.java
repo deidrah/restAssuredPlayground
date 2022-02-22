2025dd8a432bcb6ba5d35a45b149ac90e0b203e6
Deidrah <dominikastasik93@gmail.com>
2022-02-22 20:27:38 +0100

    Initial commit

---
+++ src/test/java/JsonplaceherGETTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class JsonplaceherGETTest {
+
+    //Given - konfiguracja
+    //When - wysłanie requestu
+    // Then - asercje
+
+    @Test
+    public void jsonplaceholderReadAllUsers(){
+
+    }
+}
