5bd196dec7732b088ce83cffc307259834099d3a
Deidrah <dominikastasik93@gmail.com>
2022-02-22 20:54:51 +0100

    update jsonplaceholderReadAllUsers test and add jsonplaceholderReadOneUser test

--- src/test/java/JsonplaceherGETTest.java
+++ src/test/java/JsonplaceherGETTest.java
@@ -1,6 +1,9 @@
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -15,8 +18,31 @@ public class JsonplaceherGETTest {
         Response response = given()
                 .when()
                 .get("https://jsonplaceholder.typicode.com/users");
-        System.out.print(response);
+        //System.out.print(response.asString());
 
         Assertions.assertEquals(200, response.statusCode());
+
+        JsonPath json = response.jsonPath();
+        List<Object> names = json.getList("name");
+
+        //names.stream()
+        //        .forEach(System.out::println);
+
+        Assertions.assertEquals(10, names.size());
+    }
+
+    @Test
+    public void jsonplaceholderReadOneUser() {
+        Response response = given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/users/1");
+
+        Assertions.assertEquals(200, response.statusCode());
+
+        JsonPath json = response.jsonPath();
+        Assertions.assertEquals("Leanne Graham", json.get("name"));
+        Assertions.assertEquals("Bret", json.get("username"));
+        Assertions.assertEquals("Kulas Light", json.get("address.street"));
+        //System.out.print(response.asString());
     }
 }
