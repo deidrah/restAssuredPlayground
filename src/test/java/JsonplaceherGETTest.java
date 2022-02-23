93784d1c4f179038b9920ea6cf7494d44487760e
Deidrah <dominikastasik93@gmail.com>
2022-02-23 18:42:55 +0100

    add pathvariable and query param tests

--- src/test/java/JsonplaceherGETTest.java
+++ src/test/java/JsonplaceherGETTest.java
@@ -45,4 +45,34 @@ public class JsonplaceherGETTest {
         Assertions.assertEquals("Kulas Light", json.get("address.street"));
         //System.out.print(response.asString());
     }
+
+    @Test
+    public void jsonplaceholderReadOneUserWithPathVariable() {
+        Response response = given().pathParam("userId", 1)
+                .when()
+                .get("https://jsonplaceholder.typicode.com/users/{userId}");
+
+        Assertions.assertEquals(200, response.statusCode());
+
+        JsonPath json = response.jsonPath();
+        Assertions.assertEquals("Leanne Graham", json.get("name"));
+        Assertions.assertEquals("Bret", json.get("username"));
+        Assertions.assertEquals("Kulas Light", json.get("address.street"));
+        //System.out.print(response.asString());
+    }
+
+    @Test
+    public void jsonplaceholderReadOneUserWithQueryParams() {
+        Response response = given().queryParam("username", "Bret")
+                .when()
+                .get("https://jsonplaceholder.typicode.com/users");
+
+        Assertions.assertEquals(200, response.statusCode());
+
+        JsonPath json = response.jsonPath();
+        Assertions.assertEquals("Leanne Graham", json.getList("name").get(0));
+        Assertions.assertEquals("Bret", json.getList("username").get(0));
+        Assertions.assertEquals("Kulas Light", json.getList("address.street").get(0));
+        //System.out.print(response.asString());
+    }
 }
